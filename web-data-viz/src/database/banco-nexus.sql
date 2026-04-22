796fae37988db1825e6cd243ec81dab7c405d6d4
Cesar409-dot <cesar.vequiato@sptech.school>
2026-04-22 17:08:15 -0300

    fix: nomes dos campos arrumados no script sql

--- web-data-viz/src/database/banco-nexus.sql
+++ web-data-viz/src/database/banco-nexus.sql
@@ -3,18 +3,18 @@ USE NEXUS;
 
 -- TABELAS
 CREATE TABLE Universidade (
-    idUniversidade       INT PRIMARY KEY AUTO_INCREMENT,
+    id       INT PRIMARY KEY AUTO_INCREMENT,
     cnpj                 VARCHAR(18)  NOT NULL UNIQUE,
     razaoSocial          VARCHAR(45)  NOT NULL,
     nomeFantasia         VARCHAR(45)  NOT NULL,
-    emailUniversidade    VARCHAR(45)  NOT NULL UNIQUE
+    email    VARCHAR(45)  NOT NULL UNIQUE
 );
 
 CREATE TABLE Usuario (
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    nomeUsuario      	VARCHAR(45)  NOT NULL,
-    sobrenomeUsuario 	VARCHAR(45)  DEFAULT NULL,
-    emailUsuario     	VARCHAR(45)  NOT NULL UNIQUE,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome      	VARCHAR(45)  NOT NULL,
+    sobrenome 	VARCHAR(45)  DEFAULT NULL,
+    email     	VARCHAR(45)  NOT NULL UNIQUE,
     senha              	VARCHAR(255) NOT NULL,
     token              	CHAR(6)      NOT NULL,
     cargo 				VARCHAR(45),
@@ -25,17 +25,17 @@ CREATE TABLE Usuario (
 
 CREATE TABLE Aluno (
 	RA INT PRIMARY KEY AUTO_INCREMENT,
-    nomeAluno VARCHAR(45),
-    sobrenomeAluno VARCHAR(45),
-    cpfAluno VARCHAR(11),
+    nome VARCHAR(45),
+    sobrenome VARCHAR(45),
+    cpf VARCHAR(11),
     sexo VARCHAR(1),
-    emailAluno VARCHAR(45),
+    email VARCHAR(45),
     CONSTRAINT chk_sexo CHECK (sexo IN ('F', 'M', 'O'))
 );
 
 CREATE TABLE Curso (
-    idCurso INT PRIMARY KEY AUTO_INCREMENT,
-    nomeCurso VARCHAR(45),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
     modalidade VARCHAR(45),
     mensalidade DECIMAL(6,2),
     periodo VARCHAR(20),
@@ -44,15 +44,15 @@ CREATE TABLE Curso (
 );
 
 CREATE TABLE Disciplina (
-    idDisciplina INT PRIMARY KEY AUTO_INCREMENT,
-    nomeDisciplina VARCHAR(45),
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
     cargaHoraria VARCHAR(45),
     fkCurso INT,
         FOREIGN KEY (fkCurso) REFERENCES Curso (idCurso)
 );
 
 CREATE TABLE Pagamento (
-    idPagamento INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     mesReferencia DATE,
     valor DECIMAL(6,2),
     statusPagamento VARCHAR(45),
@@ -62,7 +62,7 @@ CREATE TABLE Pagamento (
 );
 
 CREATE TABLE IndicadorRisco (
-    idIndicadorRisco INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     score DECIMAL (5,2),
     nivel VARCHAR(20),
     dataCalculo DATE,
@@ -72,7 +72,7 @@ CREATE TABLE IndicadorRisco (
 );
 
 CREATE TABLE Matricula (
-    idMatricula INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fkCurso INT,
         FOREIGN KEY (fkCurso) REFERENCES Curso (idCurso),
     fkAluno INT,
@@ -85,7 +85,7 @@ CREATE TABLE Matricula (
 );
 
 CREATE TABLE Historico (
-    idHistorico INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     fkAluno INT,
         FOREIGN KEY (fkAluno) REFERENCES Aluno (RA),
     fkDisciplina INT,
@@ -96,7 +96,7 @@ CREATE TABLE Historico (
 );
 
 CREATE TABLE Logs(
-	idLogs INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     mensagem VARCHAR(250),
     dataHora DATETIME
 );
