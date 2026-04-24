666f96493d810ef978147aa220fb01a0f47d9947
Cesar409-dot <cesar.vequiato@sptech.school>
2026-04-24 00:43:25 -0300

    fix: classe ConexaoBD alterada

--- JAR/src/main/java/sptech/school/ConexaoBD.java
+++ JAR/src/main/java/sptech/school/ConexaoBD.java
@@ -12,7 +12,7 @@ public class ConexaoBD {
         BasicDataSource conexao = new BasicDataSource();
 
         conexao.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        conexao.setUrl("jdbc:mysql://127.0.0.1:3306/NEXUS");
+        conexao.setUrl("jdbc:mysql://ContainerBD:3306/NEXUS");
         conexao.setUsername("nexus");
         conexao.setPassword("N&xus12345");
 
