8acf0ca5ae7c4015ed168357c54046b0f1ca085c
Cesar409-dot <cesar.vequiato@sptech.school>
2026-05-18 19:29:49 -0300

    fix:Atualização do script do banco de dados

--- web-data-viz/src/database/banco-nexus.sql
+++ web-data-viz/src/database/banco-nexus.sql
@@ -1,4 +1,3 @@
-drop DATABASE NEXUS;
 
 CREATE DATABASE NEXUS;
 USE NEXUS;
@@ -21,7 +20,7 @@ CREATE TABLE Usuario (
     senha VARCHAR(255) NOT NULL,
     token CHAR(6) NOT NULL,
     cargo VARCHAR(45),
-    CONSTRAINT chk_cargo CHECK (cargo IN('Coordenador', 'Professor')),
+    CONSTRAINT chk_cargo CHECK (cargo IN('Diretor', 'Coordenador')),
     fkUniversidade INT NOT NULL,
         FOREIGN KEY (fkUniversidade) REFERENCES Universidade(id)
 );
@@ -40,28 +39,8 @@ CREATE TABLE Curso (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
     modalidade VARCHAR(45),
-    mensalidade DECIMAL(6,2),
-    periodo VARCHAR(20),
     fkUniversidade INT,
         FOREIGN KEY (fkUniversidade) REFERENCES Universidade (id)
-);
-
-CREATE TABLE Disciplina (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45),
-    cargaHoraria VARCHAR(45),
-    fkCurso INT,
-        FOREIGN KEY (fkCurso) REFERENCES Curso (id)
-);
-
-CREATE TABLE Pagamento (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    mesReferencia DATE,
-    valor DECIMAL(6,2),
-    statusPagamento VARCHAR(45),
-    fkAluno INT,
-        FOREIGN KEY (fkAluno) REFERENCES Aluno (RA),
-    CONSTRAINT chk_statusPagamento CHECK (statusPagamento IN ('Pago', 'Pendente', 'Atrasado'))
 );
 
 CREATE TABLE IndicadorRisco (
@@ -74,28 +53,15 @@ CREATE TABLE IndicadorRisco (
     CONSTRAINT chk_nivel CHECK (nivel IN ('Baixo', 'Medio', 'Alto'))
 );
 
-CREATE TABLE Matricula (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fkCurso INT,
-        FOREIGN KEY (fkCurso) REFERENCES Curso (id),
-    fkAluno INT,
-        FOREIGN KEY (fkAluno) REFERENCES Aluno (RA),
-    data_ingresso DATE,
-    evadiu TINYINT,
-    CONSTRAINT chk_evadiu CHECK (evadiu IN (0, 1)),
-    motivoEvasao VARCHAR(45),
-    dataEvasao DATE
-);
-
 CREATE TABLE Historico (
     id INT PRIMARY KEY AUTO_INCREMENT,
     fkAluno INT,
         FOREIGN KEY (fkAluno) REFERENCES Aluno (RA),
-    fkDisciplina INT,
-        FOREIGN KEY (fkDisciplina) REFERENCES Disciplina (id),
     semestre INT,
     nota DECIMAL(5,2),
-    frequencia DECIMAL(5,2)
+    frequencia DECIMAL(5,2),
+    fkCurso INT,
+    FOREIGN KEY (fkCurso) REFERENCES Curso (id)
 );
 
 CREATE TABLE Logs(
@@ -103,7 +69,3 @@ CREATE TABLE Logs(
     mensagem VARCHAR(250),
     dataHora DATETIME
 );
-
-select * from usuario;
-select * from universidade;
-select * from aluno;
